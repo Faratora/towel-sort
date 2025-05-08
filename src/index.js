9c770010b4fcdcb298d948d71d6199f3a30860db
Svetlana <brigantina@mail.ru>
2025-05-08 16:48:46 +0300

    Update index.js
    fix error

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-// module.exports =
-function towelSort(matrix) {
+module.exports = function towelSort(matrix) {
   const array = [];
 
-  for (let i = 0; i < matrix.length; i++) {
-    console.log(i);
-
+  for (let i = 0; i < matrix.length; i + 1) {
     if (i % 2 === 0) {
       array.push(matrix[i]);
     } else {
       array.push(matrix[i].reverse());
     }
   }
-  return array;
-}
-// Пример использования
-console.log(towelSort(matrix)); // [1, 2, 3, 6, 5, 4, 7, 8, 9]
+  return array.flat();
+};
