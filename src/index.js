6b6e0a1e9bbfc27d0e8e6941f9cef21dd64658b5
Svetlana <brigantina@mail.ru>
2025-05-08 16:23:35 +0300

    fix error
    fix fanction error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
-};
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+// module.exports =
+function towelSort(matrix) {
+  const array = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(i);
+
+    if (i % 2 === 0){
+      array.push(matrix[i])
+    } else {
+      array.push((matrix[i]).revese());
+    }
+  }
+  return array;
+}
+// Пример использования
+
+
+console.log(towelSort(matrix)); // [1, 2, 3, 6, 5, 4, 7, 8, 9]
