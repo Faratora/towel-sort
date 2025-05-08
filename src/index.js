26e8eb1769f3e38bd4fc7a8f14ce078d1d4c30fa
Svetlana <brigantina@mail.ru>
2025-05-08 16:25:26 +0300

    Update index.js
    function finish

--- src/index.js
+++ src/index.js
@@ -10,15 +10,13 @@ function towelSort(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     console.log(i);
 
-    if (i % 2 === 0){
-      array.push(matrix[i])
+    if (i % 2 === 0) {
+      array.push(matrix[i]);
     } else {
-      array.push((matrix[i]).revese());
+      array.push(matrix[i].reverse());
     }
   }
   return array;
 }
 // Пример использования
-
-
 console.log(towelSort(matrix)); // [1, 2, 3, 6, 5, 4, 7, 8, 9]
