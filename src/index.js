a32bcaf876c7965baf308bf61aa154dc441741cd
Svetlana <brigantina@mail.ru>
2025-08-19 20:07:41 +0300

    feat:fihed eroor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 module.exports = function towelSort(matrix) {
-  const array = [];
-  if (!matrix || matrix.length === 0) return [];
-
-  for (let i = 0; i < matrix.length; i + 1) {
-    if (i % 2 === 0) {
-      array.push(matrix[i]);
-    } else {
-      array.push(matrix[i].reverse());
-    }
+  if (!matrix || matrix.length === 0) {
+    return [];
   }
-  return array.flat();
+  const result = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    const line = i % 2 === 0 ? matrix[i] : matrix[i].slice().reverse();
+    result.push(...line);
+  }
+  return result;
 };
+const matrix = [
+  [1, 2],
+  [3, 4],
+];
